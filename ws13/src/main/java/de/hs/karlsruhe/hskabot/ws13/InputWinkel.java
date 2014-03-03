fe1ad948858c9e20bbcaf2cc4bfc9c61a9618f5b
Andreas Zacher <github@azacher.de>
2014-03-03 22:41:11 +0100

    Initial commit
    Neues Projekt

---
+++ ws13/src/main/java/de/hs/karlsruhe/hskabot/ws13/InputWinkel.java
@@ -0,0 +1,15 @@
+package de.hs.karlsruhe.hskabot.ws13;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class InputWinkel extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_winkel);
+
+    }
+}
